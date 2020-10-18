d746a43c7d928419f257ffb68b070fd9fa50f1db
dickysurya33 <72327356+dickysurya33@users.noreply.github.com>
2020-10-18 14:53:19 +0700

    Latihan 1
    Praktikum 4 Latihan 1

---
+++ Praktikum 4/latihan01.py
@@ -0,0 +1,18 @@
+import math
+# waktu sewa
+awal1 = 6
+awal2 = 0
+akhir1 = 23
+akhir2 = 50
+
+# tarif sewa
+tarif_awal = 200000
+tarif_lanjutan = 10000
+tarif_menit = 10000/60
+
+# penghitungan waktu sewa
+waktupinjam = math.floor(akhir1 - awal1 + (akhir2/60) - (awal2/60))
+
+# penghitungan total sewa
+print("Biaya Sewa Anda Adalah = Rp.",
+      (waktupinjam - 12)*tarif_lanjutan + tarif_awal)
