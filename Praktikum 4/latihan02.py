b9ce77eae5b93bda554a0e900a008fbe1318926a
dickysurya33 <72327356+dickysurya33@users.noreply.github.com>
2020-10-18 15:11:51 +0700

    latihan02

---
+++ Praktikum 4/latihan02.py
@@ -0,0 +1,11 @@
+# jarak tempuh
+jarak = 795
+
+# asumssi bbm
+asumsi_bensin = 12
+
+# penghitungan bensin
+bensin_dibutuhkan = jarak / asumsi_bensin
+
+# total bensin yang dibutuhkan
+print("bensin yang dibutuhkan(lt)= ", (bensin_dibutuhkan))
